1390a8767070d48c4fe779a221588a4d3dff9c6f
4igle <80580619+Th3o-D@users.noreply.github.com>
2022-11-27 12:58:01 +0100

    add banque

--- src/cartes/batiment/vert/BanqueDeMinivilles.cpp
+++ src/cartes/batiment/vert/BanqueDeMinivilles.cpp
@@ -9,3 +9,20 @@ BanqueDeMinivilles::BanqueDeMinivilles(const string& path_image)
                    {5, 6},
                    "commerce") {}
 
+void BanqueDeMinivilles::declencher_effet(vector<Joueur *> &tab_joueurs, unsigned int joueur_actuel) {
+    /// Effet de l'BanqueDeMinivilles
+
+    // Le don de 5 pièces est géré lors de l'achat de la carte
+
+    /// TRANSACTION AVEC LA BANQUE
+    // Le joueur actuel paye 2 pièces à la banque
+    Joueur* j_actuel = tab_joueurs[joueur_actuel];
+
+    if (j_actuel->get_argent() >= 2) {
+        j_actuel->set_argent(j_actuel->get_argent() - 2);
+    }
+    else {
+        j_actuel->set_argent(0);
+    }
+
+}
--- src/cartes/batiment/vert/BanqueDeMinivilles.h
+++ src/cartes/batiment/vert/BanqueDeMinivilles.h
@@ -2,13 +2,16 @@
 #define MACHI_KORO_BANQUE_DE_MINIVILLES_H
 
 #include "../Batiment.h"
+#include "Joueur.h"
+#include "../controleur/utils.h"
+#include <list>
+#include <vector>
 
 class BanqueDeMinivilles : public Batiment {
 public:
     BanqueDeMinivilles(const string& path_image);
-    /* A faire plus tard
-    virtual void declencher_effet(*Joueur[] tab_joueurs);
-     */
+    virtual void declencher_effet(vector<Joueur*>& tab_joueurs, unsigned int joueur_actuel);
+
 };
 
 #endif //MACHI_KORO_BANQUE_DE_MINIVILLES_H
