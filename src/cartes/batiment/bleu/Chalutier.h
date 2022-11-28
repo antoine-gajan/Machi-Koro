3986db2d283c39594ad074b5c24ed5804b2ca724
Nicolas Taupin <62623511+Saucisse2toulouse@users.noreply.github.com>
2022-11-28 23:02:22 +0100

    Update Chalutier.h

--- src/cartes/batiment/bleu/Chalutier.h
+++ src/cartes/batiment/bleu/Chalutier.h
@@ -4,12 +4,13 @@
 #include "Port.h"
 #include "../Batiment.h"
 #include "Joueur.h"
+#include "utils.h"
 #include <list>
 #include <vector>
 
 class Chalutier : public Batiment{
 public:
     Chalutier(const string& path_image);
-    virtual void declencher_effet(Joueur* joueur_actuel);
+    virtual void declencher_effet(Joueur* joueur_actuel,unsigned int val_des);
 };
 #endif //MACHI_KORO_MASTER_CHALUTIER_H
