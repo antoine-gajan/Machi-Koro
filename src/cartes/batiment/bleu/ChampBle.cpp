9b31be633d6a4ad846e25df5807aea9363c32511
Nicolas Taupin <62623511+Saucisse2toulouse@users.noreply.github.com>
2022-11-28 23:13:17 +0100

    Update ChampBle.cpp

--- src/cartes/batiment/bleu/ChampBle.cpp
+++ src/cartes/batiment/bleu/ChampBle.cpp
@@ -1,13 +1,8 @@
 #include "ChampBle.h"
 
-ChampBle::ChampBle(const string &path_image) :
-        Batiment("Champ de blé",
-                 0,
-                 "Recevez une pièce de la banque",
-                 path_image,
-                 Bleu,
-                 list<unsigned int>{1},
-                 "Champ"){};
+ChampBle::ChampBle(const string &path_image) : Batiment("Champ de blé",0
+        ,"Recevez une pièce de la banque",path_image,Bleu
+        ,list<unsigned int>{1},"Champ"){};
 ///Constructeur de ChampBle
 
 void ChampBle::declencher_effet(Joueur *joueur_affecté) {
