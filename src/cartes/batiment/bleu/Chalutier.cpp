e343f7e66f8c0c11549f609690150e1f91349439
Nicolas Taupin <62623511+Saucisse2toulouse@users.noreply.github.com>
2022-11-28 23:19:33 +0100

    Update Chalutier.cpp

--- src/cartes/batiment/bleu/Chalutier.cpp
+++ src/cartes/batiment/bleu/Chalutier.cpp
@@ -1,10 +1,12 @@
 #include "Chalutier.h"
 
-Chalutier::Chalutier(const string &path_image) : Batiment("Chalutier",5
-        ,"Le joueur dont c'est le tour lance deux dés. Si vous avez le Port, recevez de la banque autant de pièces que le total des deux dés",
+Chalutier::Chalutier(const string &path_image) : 
+        Batiment("Chalutier",
+        5,
+        "Le joueur dont c'est le tour lance deux dés. Si vous avez le Port, recevez de la banque autant de pièces que le total des deux dés",
         path_image,
-        Bleu
-        ,list<unsigned int>{12,13,14},
+        Bleu,
+        list<unsigned int>{12,13,14},
         "Bateau"){};
 ///Constructeur de Chalutier
 
