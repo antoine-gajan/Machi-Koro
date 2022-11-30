bf427c99920551600e1e259ef76ddb2a4b4ed2ea
sacha-sz <102785703+sacha-sz@users.noreply.github.com>
2022-11-30 10:24:44 +0100

    Update : Cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,15 @@
 #include <iostream>
 
 /// Importation des classes monuments
-#include "Batiment.h"
-#include "BanqueDeMinivilles.h"
-#include "Boulangerie.h"
-#include "EntrepriseDeDemenagement.h"
-#include "EntrepriseDeTravauxPublics.h"
-#include "Epicerie.h"
-#include "FabriqueDeMeubles.h"
-#include "Fleuriste.h"
-#include "Fromagerie.h"
-#include "HalleDeMarche.h"
-#include "MarcheDeFruitsEtLegumes.h"
-#include "MoonsterSoda.h"
-#include "Superette.h"
+#include "Monument.h"
+#include "Port.h"
 
 using namespace std;
 
-int main()
-{
-    BanqueDeMinivilles banqueDeMinivilles;
-    banqueDeMinivilles.declencher_effet();
+int main() {
+    Port x;
+    x.declencher_effet();
 
-    Boulangerie boulangerie;
-    boulangerie.declencher_effet();
-
-    EntrepriseDeDemenagement entrepriseDeDemenagement;
-    entrepriseDeDemenagement.declencher_effet();
-
-    EntrepriseDeTravauxPublics entrepriseDeTravauxPublics;
-    entrepriseDeTravauxPublics.declencher_effet();
-
-    Epicerie epicerie;
-    epicerie.declencher_effet();
-
-    FabriqueDeMeubles fabriqueDeMeubles;
-    fabriqueDeMeubles.declencher_effet();
-
-    Fleuriste fleuriste;
-    fleuriste.declencher_effet();
-
-    Fromagerie fromagerie;
-    fromagerie.declencher_effet();
-
-    HalleDeMarche halleDeMarche;
-    halleDeMarche.declencher_effet();
-
-    MarcheDeFruitsEtLegumes marcheDeFruitsEtLegumes;
-    marcheDeFruitsEtLegumes.declencher_effet();
-
-    MoonsterSoda moonsterSoda;
-    moonsterSoda.declencher_effet();
-
-    Superette superette;
-    superette.declencher_effet();
 
     return 0;
 }
