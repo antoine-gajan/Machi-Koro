f022f6795734c5ff5ea1ce3aeaf229b65b9d93f2
sacha-sz <102785703+sacha-sz@users.noreply.github.com>
2022-12-02 15:02:53 +0100

    UPDATE : EditionDeJeu.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
-
+#include "EditionDeJeu.h"
 
 int main() {
+    EditionDeJeu edj("Deluxe");
+
+    cout << edj.get_nom() << endl;
+    auto bat = edj.get_batiment();
+    cout << "Batiments: " << bat.size() << endl;
+    for (auto it = bat.begin(); it != bat.end(); ++it) {
+        cout << it->first->get_nom() << " : " << it->second << endl;
+    }
+
+    cout << "\n\nMonuments: " << edj.get_monument().size() << endl;
+    auto mon = edj.get_monument();
+    for (auto it = mon.begin(); it != mon.end(); ++it) {
+        cout << (*it)->get_nom() << endl;
+    }
+
+
     return 0;
 }
