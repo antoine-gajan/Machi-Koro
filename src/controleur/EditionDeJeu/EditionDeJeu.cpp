5e61bbc8396df902dc75211efb893f16dce45467
sacha-sz <102785703+sacha-sz@users.noreply.github.com>
2022-12-02 15:16:13 +0100

    UPDATE : Joueur

--- src/controleur/EditionDeJeu/EditionDeJeu.cpp
+++ src/controleur/EditionDeJeu/EditionDeJeu.cpp
@@ -60,4 +60,43 @@ EditionDeJeu::EditionDeJeu(const string& nom_edition) {
         batiment.insert(pair<Batiment*, unsigned int>(new Restaurant5Etoiles(), 5));
         batiment.insert(pair<Batiment*, unsigned int>(new RestaurantFamilial(), 5));
     }
+    else if (nom_edition == "Standard") {
+        /// Constructeur de Standard
+        nom = "Standard";
+        est_edition = true;
+        nb_joueurs_max = 4;
+        nb_joueurs_min = 2;
+        nb_monuments_win = 4;
+
+        /// Liste des monuments spécifiques à Standard
+        monument.push_back(new Aeroport());
+        monument.push_back(new CentreCommercial());
+        monument.push_back(new Gare());
+        monument.push_back(new ParcAttraction());
+        monument.push_back(new TourRadio());
+
+        /// Liste des batiments spécifiques à Standard
+        // Batiments violets
+        batiment.insert(pair<Batiment*, unsigned int>(new CentreAffaires(), 4));
+        batiment.insert(pair<Batiment*, unsigned int>(new ChaineTelevision(), 4));
+        batiment.insert(pair<Batiment*, unsigned int>(new Stade(), 4));
+
+        // Batiments verts
+        batiment.insert(pair<Batiment*, unsigned int>(new Boulangerie(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new Fromagerie(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new FabriqueDeMeubles(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new MarcheDeFruitsEtLegumes(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new Superette(), 6));
+
+        // Batiments bleus
+        batiment.insert(pair<Batiment*, unsigned int>(new ChampBle(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new Ferme(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new Foret(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new Mine(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new Verger(), 6));
+
+        // Batiments rouges
+        batiment.insert(pair<Batiment*, unsigned int>(new Cafe(), 6));
+        batiment.insert(pair<Batiment*, unsigned int>(new RestaurantFamilial(), 6));
+    }
 }
