58a503f2d164e55495765042d96830083d2d8244
sacha-sz <102785703+sacha-sz@users.noreply.github.com>
2022-12-02 14:40:00 +0100

    UPDATE : EditionDeJeu.h

--- src/controleur/EditionDeJeu/EditionDeJeu.cpp
+++ src/controleur/EditionDeJeu/EditionDeJeu.cpp
@@ -1,22 +1,75 @@
 #include "EditionDeJeu.h"
+/// Importation des classes monuments
+#include "Aeroport.h"
+#include "CentreCommercial.h"
+#include "FabriqueDuPereNoel.h"
+#include "Gare.h"
+#include "HotelDeVille.h"
+#include "ParcAttraction.h"
+#include "Port.h"
+#include "TourRadio.h"
 
-EditionDeJeu::EditionDeJeu(const string& nom, unsigned int joueurs_min, unsigned int joueurs_max, unsigned int mon_win, vector<Monument*> monuments, map<Batiment*, unsigned int> batiments, bool edition):
-                           nom(nom),
-                           nb_joueurs_min(joueurs_min),
-                           nb_joueurs_max(joueurs_max),
-                           nb_monuments_win(mon_win),
-                           batiments(batiments),
-                           monuments(monuments),
-                           est_edition(edition) {
+/// Importation des batiments verts
+#include "BanqueDeMinivilles.h"
+#include "Boulangerie.h"
+#include "EntrepriseDeDemenagement.h"
+#include "EntrepriseDeTravauxPublics.h"
+#include "Epicerie.h"
+#include "FabriqueDeMeubles.h"
+#include "Fleuriste.h"
+#include "Fromagerie.h"
+#include "HalleDeMarche.h"
+#include "MarcheDeFruitsEtLegumes.h"
+#include "MoonsterSoda.h"
+#include "Superette.h"
+
+/// Importation des batiments bleus
+#include "Chalutier.h"
+#include "ChampBle.h"
+#include "ChampFleur.h"
+#include "ChampMais.h"
+#include "Ferme.h"
+#include "Foret.h"
+#include "Mine.h"
+#include "Verger.h"
+#include "Vignoble.h"
+
+/// Importation des batiments rouges
+#include "Cafe.h"
+#include "ClubPrive.h"
+#include "Pizzeria.h"
+#include "Restaurant5Etoiles.h"
+#include "RestaurantFamilial.h"
+#include "StandDeHamburger.h"
+#include "SushiBar.h"
+
+/// Importation des batiments violets
+#include "Arboretum.h"
+#include "CentreAffaires.h"
+#include "CentreImpots.h"
+#include "ChaineTelevision.h"
+#include "EntrepriseRenovation.h"
+#include "MaisonEdition.h"
+#include "Stade.h"
+#include "Startup.h"
+
+
+EditionDeJeu::EditionDeJeu(const string& nom_edition) {
     /// Constructeur de EditionDeJeu
 
     //Gestion des erreurs
-    if (nom.empty()){
+    if (nom_edition.empty()){
         throw invalid_argument("Le nom de l'édition ne peut être vide.");
     }
 
-    if (nb_joueurs_min > nb_joueurs_max){
-        throw invalid_argument("Le nombre de joueurs minimal doit être inférieur au nombre de joueurs maximal.");
-    }
+    if (nom_edition == "Deluxe") {
+        nom = "Deluxe";
+        est_edition = true;
+        nb_joueurs_max = 5;
+        nb_joueurs_min = 2;
+        nb_monuments_win = 5;
 
+        // Liste des monuments spécifiques à Deluxe
+        mon = vector<Monument>();
+    }
 }
--- src/controleur/EditionDeJeu/EditionDeJeu.h
+++ src/controleur/EditionDeJeu/EditionDeJeu.h
@@ -13,20 +13,23 @@ protected:
     unsigned int nb_joueurs_min;
     unsigned int nb_joueurs_max;
     unsigned int nb_monuments_win;
-    map<Batiment*, unsigned int> batiments;
-    vector<Monument*> monuments;
+
+    vector<Monument> mon;
+    map<Batiment, unsigned int> bat;
+
     bool est_edition;
 
 public:
+    /// *** Constructeur et destructeur *** ///
     EditionDeJeu(const string& nom);
     EditionDeJeu() = default;
     ~EditionDeJeu()=default;
+
+    /// *** Getters *** ///
     const std::string& get_nom() const {return nom;};
     unsigned int get_nb_joueurs_min() const {return nb_joueurs_min;};
     unsigned int get_nb_joueurs_max() const {return nb_joueurs_max;};
     unsigned int get_nb_monuments_win() const {return nb_monuments_win;};
     bool get_est_edition() const {return est_edition;};
-    map<Batiment*, unsigned int> get_batiments() const {return batiments;};
-    vector<Monument*> get_monuments() const {return monuments;};
 };
 #endif //MACHI_KORO_EDITIONDEJEU_H
