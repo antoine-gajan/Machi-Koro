9e402d8e73444bdcb455328024721c9c90143c91
Antoine <antoine1.gajan@gmail.com>
2022-12-18 20:49:14 +0100

    Modif VueJoueur

--- Joueur/VueJoueur.cpp
+++ Joueur/VueJoueur.cpp
@@ -2,21 +2,41 @@
 
 VueJoueur::VueJoueur(Joueur* joueur, QWidget *parent) {
     /// Vue d'un joueur
+    // Nom du joueur
+    nom_joueur = new QLabel;
+    nom_joueur->setText(QString::fromStdString(joueur->get_nom()));
     // Argent du joueur
     argent=new QLCDNumber;
     argent->display((int)joueur->get_argent());
     argent->setFixedHeight(30);
 
     // Barre avec les informations du joueur
-    layoutInformations=new QHBoxLayout;
-    layoutBatiments=new QGridLayout;
+    layout_informations = new QHBoxLayout;
+    layout_batiments = new QGridLayout;
+    layout_monuments = new QGridLayout;
 
-    // Création des vue des cartes du joueur
+    int ind_cartes = 0;
+    int ind_couleurs = 0;
+    // Création des vues des batiments du joueur
     for (auto& couleur : joueur->get_liste_batiment()){
         for (auto bat : couleur.second){
-            // Affichage de la carte (à faire)
+            // Affichage du batiment
+            vue_batiments.push_back(new VueCarte(*bat.first, parent));
+            layout_batiments->addWidget(vue_batiments[ind_cartes], ind_couleurs, ind_cartes);
+            ind_cartes++;
         }
+        ind_cartes = 0;
+        ind_couleurs++;
     }
 
+    // Création des vues des monuments du joueur
+    for (auto& mon : joueur->get_liste_monument()){
+        vue_monuments.push_back(new VueCarte(*mon.first, parent));
+        layout_monuments->addWidget(vue_monuments[ind_cartes], ind_cartes/3, ind_cartes%3);
+        ind_cartes++;
+    }
 
+    // Ajout des layouts
+    layout_informations->addLayout(layout_monuments);
+    layout_informations->addLayout(layout_batiments);
 }
--- Joueur/VueJoueur.h
+++ Joueur/VueJoueur.h
@@ -18,12 +18,14 @@ Q_OBJECT
 public:
     explicit VueJoueur(Joueur* joueur, QWidget *parent = nullptr);
 private:
-    QLabel* nom_joueur; // texte "Score"
+    QLabel* nom_joueur; // texte avec le nom du joueur
     QLCDNumber* argent; // affichage de l'argent du joueur
-    QHBoxLayout* layoutInformations;
-    QGridLayout* layoutBatiments; // grille des batiments du joueur
-    QGridLayout* layoutMonuments; // grille des monuments du joueur
-    vector<VueCarte*> vuecartes; // adresses des objets VueCarte
+    QHBoxLayout* layout_informations;
+    QGridLayout* layout_batiments; // grille des batiments du joueur
+    QGridLayout* layout_monuments; // grille des monuments du joueur
+    vector<VueCarte*> vue_batiments; // adresses des batiments VueCarte
+    vector<VueCarte*> vue_monuments; // adresses des monuments VueCarte
+
 private slots:
     // slots qui gère les clics sur les cartes
     //void carteClique(VueCarte* vc);
