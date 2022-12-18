d4ceb1f11125da5b17ae39d2dae108e7a9fea7e0
Antoine <antoine1.gajan@gmail.com>
2022-12-18 12:22:27 +0100

    Constructeur de VueJoueur

--- Joueur/VueJoueur.cpp
+++ Joueur/VueJoueur.cpp
@@ -1,1 +1,22 @@
 #include "VueJoueur.h"
+
+VueJoueur::VueJoueur(Joueur* joueur, QWidget *parent) {
+    /// Vue d'un joueur
+    // Argent du joueur
+    argent=new QLCDNumber;
+    argent->display((int)joueur->get_argent());
+    argent->setFixedHeight(30);
+
+    // Barre avec les informations du joueur
+    layoutInformations=new QHBoxLayout;
+    layoutBatiments=new QGridLayout;
+
+    // Création des vue des cartes du joueur
+    for (auto& couleur : joueur->get_liste_batiment()){
+        for (auto bat : couleur.second){
+            // Affichage de la carte (à faire)
+        }
+    }
+
+
+}
--- Joueur/VueJoueur.h
+++ Joueur/VueJoueur.h
@@ -1,9 +1,32 @@
 #ifndef MACHI_KORO_VUEJOUEUR_H
 #define MACHI_KORO_VUEJOUEUR_H
 
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QProgressBar>
+#include <QLCDNumber>
+#include <QString>
+#include <QMessageBox>
+#include "VueCarte.h"
+#include "Joueur.h"
 
-class VueJoueur {
-
+class VueJoueur : public QWidget
+{
+Q_OBJECT
+public:
+    explicit VueJoueur(Joueur* joueur, QWidget *parent = nullptr);
+private:
+    QLabel* nom_joueur; // texte "Score"
+    QLCDNumber* argent; // affichage de l'argent du joueur
+    QHBoxLayout* layoutInformations;
+    QGridLayout* layoutBatiments; // grille des batiments du joueur
+    QGridLayout* layoutMonuments; // grille des monuments du joueur
+    vector<VueCarte*> vuecartes; // adresses des objets VueCarte
+private slots:
+    // slots qui gère les clics sur les cartes
+    void carteClique(VueCarte* vc);
 };
 
 
