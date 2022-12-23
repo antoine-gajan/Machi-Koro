8ffb99baad0fab3481d8540a6a7d560aee63db99
Antoine <antoine1.gajan@gmail.com>
2022-12-23 17:37:12 +0100

    Ajout classes

---
+++ controleur/VueShop.h
@@ -0,0 +1,13 @@
+#ifndef MACHI_KORO_VUESHOP_H
+#define MACHI_KORO_VUESHOP_H
+
+#include <QWidget>
+#include "Shop.h"
+
+class VueShop {
+public:
+    VueShop(const Shop& shop, QWidget *parent = nullptr);
+};
+
+
+#endif //MACHI_KORO_VUESHOP_H
